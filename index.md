51d7ae9a974bc96c229fa4e3e9e09b519fe04828
Cody Molisee <molisee.cody@gmail.com>
2026-03-29 15:22:42 -0700

    wip: removed all files. clean slate.

--- index.md
+++
@@ -1,5 +0,0 @@
----
-layout: home
-title: Writing
-description: Thoughts, essays, and observations.
----
--- test/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rspec'
-require 'yaml'
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    # Change to project root
-    Dir.chdir(File.expand_path('..', __dir__))
-  end
-end
