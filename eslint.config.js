4bbb7641b8d989db001d8d4f6ea03a2482e2d31e
Cody <37119390+cmolisee@users.noreply.github.com>
2026-04-05 04:23:13 +0000

    chore: re-configured eslint to work

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  parserOptions: {
+  languageOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   rules: {
     // Errors
-    "no-unused-vars": "warn",
+    "no-unused-vars": "error",
     "no-console": "off",
     eqeqeq: ["error", "always"],
     curly: "error",
-
     // Style (handled by Prettier, set to off to avoid conflicts)
     semi: "off",
     quotes: "off",
